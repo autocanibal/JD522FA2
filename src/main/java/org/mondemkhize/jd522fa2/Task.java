f9b01c2809e2f2ff71a8fa4f401852e9b5fa3623
Monde <mondzzmkhize@gmail.com>
2023-09-11 21:24:36 +0200

    Added things probably

--- src/main/java/org/mondemkhize/jd522fa2/Task.java
+++ src/main/java/org/mondemkhize/jd522fa2/Task.java
@@ -4,6 +4,18 @@
  */
 package org.mondemkhize.jd522fa2;
 
+import java.awt.Component;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.nio.file.*;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author monde
@@ -44,5 +56,35 @@ public class Task {
 
     public void setCompletionState(boolean completionState) {
         this.completionState = completionState;
+    }
+    
+    public void writeToFile(String taskName, String category, String description, String completionState, Component comp){
+        BufferedWriter out = null;
+        try {
+            out = new BufferedWriter(new FileWriter("out.txt"));
+            out.write(taskName + "\n" + category + "\n" + description + "\n" + completionState);
+            new JOptionPane().showMessageDialog(comp, "It worked");
+        } catch (IOException ex) {
+            new JOptionPane().showMessageDialog(comp, "IO Failed");
+        } finally {
+            try {
+                out.close();
+            } catch (IOException ex) {
+                Logger.getLogger(NewTaskForm.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public List<String> readFromFile(Component comp){
+        Path filePath = Path.of("out.txt");
+        try{
+            List<String> contents = Files.readAllLines(filePath);
+            //contents.forEach(line -> System.out.println(line));
+            return contents;
+        }catch (IOException ex) {
+            new JOptionPane().showMessageDialog(comp, ex.getMessage());
+            return null;
+        }
+        
     }
 }
